e4722d85b798ba46b9c589ea643e19764f2abe86
Cottand <nico.dc@outlook.com>
2023-11-02 00:10:06 +0000

    move nomad.nix to modules modler, deploy arman's webdav

--- machines/nico-xps/vscode.nix
+++ machines/nico-xps/vscode.nix
@@ -13,14 +13,24 @@
         vscodevim.vim
         jnoortheen.nix-ide
         hashicorp.terraform
-        hashicorp.hcl
-
       ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
         {
           name = "remote-ssh-edit";
           publisher = "ms-vscode-remote";
           version = "0.47.2";
           sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+        }
+        {
+          name = "HCL";
+          publisher = "HashiCorp";
+          version = "0.3.2";
+          sha256 = "sha256-cxF3knYY29PvT3rkRS8SGxMn9vzt56wwBXpk2PqO0mo=";
+        }
+        {
+          name = "custom-format";
+          publisher = "Vehmloewff";
+          version = "1.0.2";
+          sha256 = "sha256-yx74XxLdvykwyd5pardJT9W1BF7EIGVV0uoD4Z56bww=";
         }
       ];
     })
--- modules/default.nix
+++ modules/default.nix
@@ -1,5 +1,6 @@
 {
     imports = [
         ./wireguard.nix
+        ./nomad.nix
     ];
 }
