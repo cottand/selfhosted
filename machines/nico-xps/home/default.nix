87f38fa9b7c709753d389841377fc2f06cc4d9d9
Nico D'Cotta <nico.dc@outlook.com>
2023-11-08 12:55:27 +0000

    set up ideavimrc

--- machines/nico-xps/home/default.nix
+++ machines/nico-xps/home/default.nix
@@ -33,5 +33,31 @@
     };
   };
 
+    home.file.".ideavimrc" = {
+      text = ''
+" Show a few lines of context around the cursor. Note that this makes the
+" text scroll if you mouse-click near the start or end of the window.
+set scrolloff=5
+
+" Do incremental searching.
+set incsearch
+
+" Don't use Ex mode, use Q for formatting.
+map Q gq
+
+
+
+"" -- Map IDE actions to IdeaVim -- https://jb.gg/abva4t
+map gf <Action>(ReformatCode)
+
+map gr <Action>(Run)
+"" Map \b to toggle the breakpoint on the current line
+"map \b <Action>(ToggleLineBreakpoint)
+
+
+" Find more examples here: https://jb.gg/share-ideavimrc
+      '';
+    };
+
   home.stateVersion = "22.11";
 }
