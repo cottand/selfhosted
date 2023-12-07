f1266604c6286a2d9074b948e88df412041e5614
Cottand <nico.dc@outlook.com>
2023-12-07 20:33:29 +0000

    flake-ify nico-xps

---
+++ flake.nix
@@ -0,0 +1,153 @@
+{
+  inputs = {
+    nixpkgs23-11.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs23-05-pinned.url = "github:NixOS/nixpkgs/d2e4de209881b38392933fabf303cde3454b0b4c";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    cottand = {
+      url = "github:cottand/home-nix";
+      inputs.nixpgs-unstable.follows = "nixpkgs-unstable";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs23-11";
+    };
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+  };
+
+  outputs = inputs@{ nixpkgs23-11, nixpkgs23-05-pinned, cottand, home-manager, nixos-hardware, ... }:
+    let
+      overlay = cottand.overlay;
+    in
+    {
+      colmena = {
+        meta = {
+          nixpkgs = import nixpkgs23-05-pinned {
+            system = "x86_64-linux";
+          };
+
+          nodeNixpkgs = {
+            nico-xps = import nixpkgs23-11 {
+              system = "x86_64-linux";
+              config.allowUnfree = true;
+            };
+          };
+        };
+
+        defaults = { pkgs, lib, name, nodes, ... }: {
+          imports = [
+            ./machines/${name}/definition.nix
+            ./machines/common_config.nix
+            ./modules
+          ];
+          nixpkgs.overlays = [ overlay ];
+          nixpkgs.system = "x86_64-linux";
+          networking.hostName = lib.mkDefault name;
+
+          deployment.replaceUnknownProfiles = lib.mkDefault true;
+          deployment.buildOnTarget = lib.mkDefault true;
+          deployment.targetHost = lib.mkDefault "${name}.mesh.dcotta.eu";
+        };
+
+        nico-xps = { name, nodes, ... }: {
+          imports = [
+            home-manager.nixosModules.home-manager
+            nixos-hardware.nixosModules.dell-xps-13-9300
+          ];
+          # TEMP?
+          home-manager = {
+            useUserPackages = true;
+            useGlobalPkgs = true;
+            users.cottand = cottand.home;
+          };
+
+          deployment = {
+            # Allow local deployment with `colmena apply-local`
+            allowLocalDeployment = true;
+
+            # Disable SSH deployment. This node will be skipped in a
+            # normal`colmena apply`.
+            targetHost = null;
+          };
+        };
+
+        cosmo = { name, nodes, ... }: {
+          deployment.targetHost = "${name}.vps.dcotta.eu";
+          deployment.tags = [ "contabo" "nomad-server" ];
+          custom.wireguard."wg-mesh" = {
+            enable = true;
+            confPath = secret/wg-mesh/${name}.conf;
+            port = 55820;
+          };
+        };
+
+
+        miki = { name, nodes, lib, ... }: {
+          deployment.targetHost = "${name}.vps.dcotta.eu";
+          nixpkgs.system = lib.mkForce "aarch64-linux";
+          deployment.tags = [ "hetzner" "nomad-client" ];
+          custom.wireguard."wg-mesh" = {
+            enable = true;
+            confPath = secret/wg-mesh/${name}.conf;
+            port = 55820;
+          };
+        };
+
+        ari = { name, nodes, ... }: {
+          imports = [
+            ./machines/${name}/definition.nix
+          ];
+          networking.hostName = name;
+          deployment.tags = [ "local" "nomad-server" ];
+          custom.wireguard."wg-mesh" = {
+            enable = true;
+            confPath = secret/wg-mesh/${name}.conf;
+            port = 55820;
+          };
+          deployment.targetHost = "192.168.1.44";
+        };
+
+        maco = { name, nodes, ... }: {
+          deployment.tags = [ "contabo" "nomad-server" ];
+          deployment.targetHost = "maco.mesh.dcotta.eu";
+          custom.wireguard."wg-mesh" = {
+            enable = true;
+            confPath = secret/wg-mesh/${name}.conf;
+            port = 55820;
+          };
+        };
+
+        elvis = { name, nodes, ... }: {
+          deployment.targetHost = "elvis.vps6.dcotta.eu";
+          deployment.tags = [ "local" "nomad-client" ];
+          custom.wireguard."wg-mesh" = {
+            enable = true;
+            confPath = secret/wg-mesh/${name}.conf;
+            port = 55820;
+          };
+        };
+
+        ziggy = { name, nodes, ... }: {
+          imports = [ ];
+          deployment.tags = [ "local" "nomad-client" ];
+          deployment.targetHost = "ziggy.vps6.dcotta.eu"; # TODO CHANGE
+          custom.wireguard."wg-mesh" = {
+            enable = true;
+            confPath = secret/wg-mesh/${name}.conf;
+            port = 55820;
+          };
+        };
+
+        bianco = { name, nodes, ... }: {
+          imports = [
+            ./machines/laptop_config.nix
+          ];
+          deployment.tags = [ "madrid" "nomad-client" ];
+          custom.wireguard."wg-mesh" = {
+            enable = true;
+            confPath = secret/wg-mesh/${name}.conf;
+            port = 55820;
+          };
+        };
+      };
+    };
+}
--- machines/nico-xps/hidpi.nix
+++ machines/nico-xps/hidpi.nix
@@ -16,8 +16,8 @@
   hardware.opengl = {
     enable = true;
     extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      # intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      # vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
       vaapiVdpau
       libvdpau-va-gl
     ];
