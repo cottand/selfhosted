40c5b105dddba6bbef73ba7bc13f6ff949149ad6
Cottand <nico.dc@outlook.com>
2024-06-05 20:31:54 +0100

    store cloudflare zoneIds in bitwarden

--- terraform/base/dcotta-com.tf
+++ terraform/base/dcotta-com.tf
@@ -1,21 +1,17 @@
-variable "com_zone_id" {
-  type = string
-}
-
 
 module "node_miki_com" {
-  cf_zone_id  = var.com_zone_id
+  cf_zone_id  = local.zoneIds["com"]
   source      = "../modules/node"
   name        = "miki"
   ip4_mesh    = local.mesh_ip4.miki
-  ip4_pub     = var.pub_ip4.miki
-  ip6_pub     = var.pub_ip6.miki
+  ip4_pub     = local.pubIp["ip4"]["miki"]
+  ip6_pub     = local.pubIp["ip6"]["miki"]
   is_web_ipv4 = true
   is_web_ipv6 = true
 }
 
 resource "cloudflare_record" "nico-cname-web-com" {
-  zone_id = var.com_zone_id
+  zone_id = local.zoneIds["com"]
   name    = "nico"
   type    = "CNAME"
   value   = "miki.vps.dcotta.com"
--- terraform/base/dcotta-eu.tf
+++ terraform/base/dcotta-eu.tf
@@ -1,15 +1,3 @@
-variable "eu_zone_id" {
-  type = string
-}
-
-variable "pub_ip4" {
-  type = map(string)
-}
-
-variable "pub_ip6" {
-  type = map(string)
-}
-
 locals {
   mesh_ip4 = {
     cosmo  = "10.10.0.1"
@@ -21,84 +9,86 @@ locals {
     xps2   = "10.10.6.1"
     bianco = "10.10.0.2"
   }
+  zoneIds = jsondecode(data.bitwarden-secrets_secret.zoneIds.value)
+  pubIp = jsondecode(data.bitwarden-secrets_secret.pubIps.value)
 }
 
 
 ## Discovery
 
 module "node_miki" {
-  cf_zone_id  = var.eu_zone_id
+  cf_zone_id  = local.zoneIds["eu"]
   source      = "../modules/node"
   name        = "miki"
   ip4_mesh    = local.mesh_ip4.miki
-  ip4_pub     = var.pub_ip4.miki
-  ip6_pub     = var.pub_ip6.miki
+  ip4_pub     = local.pubIp["ip4"]["miki"]
+  ip6_pub     = local.pubIp["ip6"]["miki"]
   is_web_ipv4 = true
   is_web_ipv6 = true
 }
 module "node_maco" {
-  cf_zone_id  = var.eu_zone_id
+  cf_zone_id  = local.zoneIds["eu"]
   source      = "../modules/node"
   name        = "maco"
   ip4_mesh    = local.mesh_ip4.maco
-  ip4_pub     = var.pub_ip4.maco
-  ip6_pub     = var.pub_ip6.maco
+  ip4_pub     = local.pubIp["ip4"]["maco"]
+  ip6_pub     = local.pubIp["ip6"]["maco"]
   is_web_ipv4 = true
   is_web_ipv6 = true
 }
 module "node_cosmo" {
-  cf_zone_id  = var.eu_zone_id
+  cf_zone_id  = local.zoneIds["eu"]
   source      = "../modules/node"
   name        = "cosmo"
   ip4_mesh    = local.mesh_ip4.cosmo
-  ip4_pub     = var.pub_ip4.cosmo
-  ip6_pub     = var.pub_ip6.cosmo
+  ip4_pub     = local.pubIp["ip4"]["cosmo"]
+  ip6_pub     = local.pubIp["ip6"]["cosmo"]
   is_web_ipv4 = true
   is_web_ipv6 = true
 }
 
 module "node_elvis" {
-  cf_zone_id  = var.eu_zone_id
+  cf_zone_id  = local.zoneIds["eu"]
   source      = "../modules/node"
   name        = "elvis"
   ip4_mesh    = local.mesh_ip4.elvis
   ip4_pub     = null
-  ip6_pub     = var.pub_ip6.elvis
+  ip6_pub     = local.pubIp["ip6"]["elvis"]
   is_web_ipv4 = false
   is_web_ipv6 = false
 }
 module "node_ari" {
-  cf_zone_id  = var.eu_zone_id
+  cf_zone_id  = local.zoneIds["eu"]
   source      = "../modules/node"
   name        = "ari"
   ip4_mesh    = local.mesh_ip4.ari
   ip4_pub     = null
-  ip6_pub     = var.pub_ip6.ari
+  ip6_pub     = local.pubIp["ip6"]["ari"]
   is_web_ipv4 = false
   is_web_ipv6 = false
 }
 module "node_xps2" {
-  cf_zone_id  = var.eu_zone_id
+  cf_zone_id  = local.zoneIds["eu"]
   source      = "../modules/node"
   name        = "xps2"
   ip4_mesh    = local.mesh_ip4.xps2
   ip4_pub     = null
-  ip6_pub     = var.pub_ip6.xps2
+  ip6_pub     = local.pubIp["ip6"]["xps2"]
   is_web_ipv4 = false
   is_web_ipv6 = false
 }
 module "node_ziggy" {
-  cf_zone_id  = var.eu_zone_id
+  cf_zone_id  = local.zoneIds["eu"]
   source      = "../modules/node"
   name        = "ziggy"
   ip4_mesh    = local.mesh_ip4.ziggy
   ip4_pub     = null
-  ip6_pub     = var.pub_ip6.ziggy
+  ip6_pub     = local.pubIp["ip6"]["ziggy"]
   is_web_ipv4 = false
   is_web_ipv6 = false
 }
 module "node_bianco" {
-  cf_zone_id  = var.eu_zone_id
+  cf_zone_id  = local.zoneIds["eu"]
   source      = "../modules/node"
   name        = "bianco"
   ip4_mesh    = local.mesh_ip4.bianco
@@ -109,12 +99,11 @@ module "node_bianco" {
 }
 
 
-
 # Websites
 
 
 resource "cloudflare_record" "nico-cname-web" {
-  zone_id = var.eu_zone_id
+  zone_id = local.zoneIds["eu"]
   name    = "nico"
   type    = "CNAME"
   value   = "miki.vps.dcotta.eu"
@@ -123,7 +112,7 @@ resource "cloudflare_record" "nico-cname-web" {
   proxied = true
 }
 resource "cloudflare_record" "nico-test--cname-nico" {
-  zone_id = var.eu_zone_id
+  zone_id = local.zoneIds["eu"]
   name    = "test-nico"
   type    = "CNAME"
   value   = "miki.vps.dcotta.eu"
@@ -133,7 +122,7 @@ resource "cloudflare_record" "nico-test--cname-nico" {
 }
 
 resource "cloudflare_record" "lemmy-cname-web" {
-  zone_id = var.eu_zone_id
+  zone_id = local.zoneIds["eu"]
   name    = "r"
   type    = "CNAME"
   value   = "web.dcotta.eu"
