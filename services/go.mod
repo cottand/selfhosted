a2c034d5f973104c66c76b3ae940c212a021031e
Cottand <nico.dc@outlook.com>
2024-08-10 16:46:16 +0100

    start developing bedrock

--- services/go.mod
+++ services/go.mod
@@ -2,11 +2,15 @@ module github.com/cottand/selfhosted/services
 
 go 1.22.3
 
-require github.com/prometheus/client_golang v1.19.1
+require (
+	github.com/monzo/terrors v0.0.0-20240312165114-073654f2beb9
+	github.com/prometheus/client_golang v1.19.1
+)
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/prometheus/client_model v0.5.0 // indirect
 	github.com/prometheus/common v0.48.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
--- services/lib/bedrock/init.go
+++ services/lib/bedrock/init.go
@@ -1,12 +1,53 @@
 package bedrock
 
-import "net/http"
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"strconv"
+)
 
 import (
+	"github.com/monzo/terrors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func Init() {
 	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":2112", nil)
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	slog.SetDefault(logger)
+
+	slog.Info("Bedrock initialized")
+}
+
+func GetBaseConfig() (*BaseConfig, error) {
+	port, ok := os.LookupEnv("HTTP_PORT")
+	if !ok {
+		return nil, terrors.New("no_env", "missing env config for http port", nil)
+	}
+	portNum, err := strconv.Atoi(port)
+	if err != nil {
+		return nil, terrors.Augment(err, "invalid env config for http port", nil)
+	}
+	host, ok := os.LookupEnv("HTTP_HOST")
+	if !ok {
+		return nil, terrors.New("no_env", "missing env config for http host", nil)
+	}
+
+	return &BaseConfig{
+		HttpHost: host,
+		HttpPort: portNum,
+	}, nil
+}
+
+type BaseConfig struct {
+	HttpHost string
+	HttpPort int
+}
+
+func (c *BaseConfig) HttpBind() string {
+	return fmt.Sprint(c.HttpHost, ":", strconv.Itoa(c.HttpPort))
 }
