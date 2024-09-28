773c59f625c52aeb207debb0cb3524fab34d46b0
Cottand <nico.dc@outlook.com>
2024-09-28 12:11:40 +0100

    use nix 2.23 in nixmad and services go

--- scripts/nixmad.nix
+++ scripts/nixmad.nix
@@ -15,7 +15,7 @@ buildGoModule {
   src = util.devGoSrc;
   vendorHash = null;
   nativeBuildInputs = [ pkg-config makeWrapper ];
-  buildInputs = [ nixVersions.nix_2_22 ];
+  buildInputs = [ nixVersions.nix_2_23 ];
   subPackages = [ "cmd/${name}" ];
   CGO_ENABLED = 1;
   postInstall = ''
