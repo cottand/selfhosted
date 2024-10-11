b0b6cc7c12a9252f86f149f775367792503ef273
Cottand <nico.dc@outlook.com>
2024-10-11 19:59:20 +0100

    add CA to action

--- terraform/ci/web.tf
+++ terraform/ci/web.tf
@@ -1,13 +1,19 @@
 resource "tailscale_tailnet_key" "github_actions" {
   reusable      = true
   ephemeral     = true
-  tags          = ["tag:ci/gha"]
+#   tags          = ["tag:ci"]
   preauthorized = true
 }
 
+
 resource "github_actions_secret" "tailscale_authkey" {
-  repository  = "cottand/selfhosted"
+  repository  = "selfhosted"
   secret_name = "TAILSCALE_AUTHKEY"
   plaintext_value = tailscale_tailnet_key.github_actions.key
 }
 
+resource "github_actions_secret" "root_ca" {
+  repository  = "selfhosted"
+  secret_name = "DCOTTA_ROOT_CA"
+  plaintext_value = base64encode(file("../../certs/root_2024_ca.crt"))
+}
