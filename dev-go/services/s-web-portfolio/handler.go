076ad5912a519cc3f0af9ce96b66f887175098b5
Cottand <nico.dc@outlook.com>
2024-10-12 11:02:21 +0100

    remove static website from go server

--- dev-go/services/s-web-portfolio/handler.go
+++ dev-go/services/s-web-portfolio/handler.go
@@ -8,39 +8,14 @@ import (
 )
 
 type scaffold struct {
-	fs             http.HandlerFunc
 	stats          s_rpc_portfolio_stats.PortfolioStatsClient
 	doGrpcUpstream bool
 }
 
 func (s *scaffold) MakeHandler() http.Handler {
 	mux := http.NewServeMux()
-
-	mux.Handle("/static/", s.fs)
-	mux.Handle("/assets/", s.fs)
-	mux.Handle("/styles/", s.fs)
-	mux.Handle("/robots.txt", s.fs)
-	mux.Handle("/CNAME", s.fs)
-	mux.HandleFunc("/", s.handleHTTPRoot)
 	mux.HandleFunc("/api/browse", s.handleHttpBrowse)
-
 	return mux
-}
-
-func (s *scaffold) handleHTTPRoot(rw http.ResponseWriter, req *http.Request) {
-	originalPath := req.URL.Path
-	req.URL.Path = "/"
-	s.fs.ServeHTTP(rw, req)
-
-	go func() {
-		if s.doGrpcUpstream {
-			_, _ = s.stats.Report(context.WithoutCancel(req.Context()), &s_rpc_portfolio_stats.Visit{
-				Url:       originalPath,
-				Ip:        req.Header.Get("X-Forwarded-For"),
-				UserAgent: req.Header.Get("User-Agent"),
-			})
-		}
-	}()
 }
 
 type BrowseRequest struct {
--- dev-go/services/s-web-portfolio/package.nix
+++ dev-go/services/s-web-portfolio/package.nix
@@ -4,24 +4,15 @@ let
 
   src = util.cleanSourceForGoService name;
 
-  assetsEnv = buildEnv {
-    name = "${name}-assets";
-    paths = [
-      # has files under /srv
-      (builtins.getFlake "github:cottand/web-portfolio/c5cb2fa3866f67238e51c1c2896c63c3fac56c76").packages.${system}.static
-    ];
-  };
-
   bin = buildGoModule {
     inherit name src;
     subPackages = [ "services/${name}" ];
     vendorHash = null;
-    ldflags = [ "-X github.com/cottand/selfhosted/dev-go/lib/bedrock.nixAssetsDir=${assetsEnv.outPath}" ];
   };
 
   binaryEnv = buildEnv {
     inherit name;
-    paths = [ bin assetsEnv bash ];
+    paths = [ bin bash ];
   };
 in
 binaryEnv // { inherit bin; }
