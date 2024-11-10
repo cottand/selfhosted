c2954d19a44f2dabb50dc42150d589a0421f1782
Cottand <nico.dc@outlook.com>
2024-11-10 20:45:08 +0000

    add log line to vault services snapsotter

--- dev-go/services/s-rpc-vault/handler.go
+++ dev-go/services/s-rpc-vault/handler.go
@@ -37,5 +37,6 @@ func (h *ProtoHandler) Snapshot(ctx context.Context, _ *emptypb.Empty) (*emptypb
 		return nil, terrors.Augment(e, "failed to upload snapshot", nil)
 	default:
 	}
+	slog.Info("vault snapshot uploaded successfully")
 	return &emptypb.Empty{}, terrors.Augment(err, "failed to upload snapshot", nil)
 }
