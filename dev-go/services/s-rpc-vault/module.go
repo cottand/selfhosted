ce0583d3eb7cec94b0d924a812de1912aac367b5
Cottand <nico.dc@outlook.com>
2024-11-12 20:29:32 +0000

    properly eval error

--- dev-go/services/s-rpc-vault/module.go
+++ dev-go/services/s-rpc-vault/module.go
@@ -13,7 +13,7 @@ var Name, slog, tracer = bedrock.Service("s-rpc-vault")
 func InitService() {
 	vaultConfig := vault.DefaultConfig()
 	vaultConfig.Address = "https://vault.dcotta.com:8200"
-	vaultClient, err := vault.NewClient(vault.DefaultConfig())
+	vaultClient, err := vault.NewClient(vaultConfig)
 	if err != nil {
 		slog.Error("failed to init vault client: %v", err)
 		return
