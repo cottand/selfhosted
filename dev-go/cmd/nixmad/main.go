b133f1a5aeded5c44367af5428511df8529ee7da
Cottand <nico.dc@outlook.com>
2024-11-19 00:22:28 +0000

    HA attic

--- dev-go/cmd/nixmad/main.go
+++ dev-go/cmd/nixmad/main.go
@@ -15,7 +15,7 @@ func main() {
 }
 
 var rootCmd = &cobra.Command{
-	Use:           "nixmad ./path/to/job.nix [-v VERSION]",
+	Use:           "nixmad ./path/to/job.nix",
 	Short:         "nixmad is a tool for deploying Nix-templated Nomad jobs",
 	RunE:          RunCommand,
 	Args:          cobra.MinimumNArgs(1),
