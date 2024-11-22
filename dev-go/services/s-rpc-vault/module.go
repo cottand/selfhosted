8f7fc4dc00b4a8bcc703e6d835a786c26a271e3e
Cottand <nico.dc@outlook.com>
2024-11-22 12:25:30 +0000

    properly config vault client

--- dev-go/services/s-rpc-vault/module.go
+++ dev-go/services/s-rpc-vault/module.go
@@ -4,26 +4,16 @@ import (
 	"github.com/cottand/selfhosted/dev-go/lib/bedrock"
 	"github.com/cottand/selfhosted/dev-go/lib/mono"
 	s_rpc_vault "github.com/cottand/selfhosted/dev-go/lib/proto/s-rpc-vault"
-	vault "github.com/hashicorp/vault/api"
+	"github.com/cottand/selfhosted/dev-go/lib/secretstore"
 	"google.golang.org/grpc"
 )
 
 var Name, slog, tracer = bedrock.Service("s-rpc-vault")
 
 func InitService() {
-	vaultConfig := vault.DefaultConfig()
-	vaultConfig.Address = "https://vault.dcotta.com:8200"
-	vaultConfig.DisableRedirects = false
-	err := vaultConfig.ConfigureTLS(&vault.TLSConfig{
-		TLSServerName: "vault.dcotta.com",
-	})
+	vaultClient, err := secretstore.NewClient()
 	if err != nil {
-		slog.Error("failed to init vault client: %v", err)
-		return
-	}
-	vaultClient, err := vault.NewClient(vaultConfig)
-	if err != nil {
-		slog.Error("failed to init vault client: %v", err)
+		slog.Error("failed to init vault client", "err", err.Error())
 		return
 	}
 	protoHandler := &ProtoHandler{
