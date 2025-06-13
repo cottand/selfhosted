8214b6813aa8bbf2e6eb9ad5b948c4509a057c8a
Cottand <nico.dc@outlook.com>
2025-06-13 12:22:11 +0100

    fix test noise

--- dev-go/lib/bigq/bigq.go
+++ dev-go/lib/bigq/bigq.go
@@ -3,7 +3,6 @@ package bigq
 import (
 	"cloud.google.com/go/bigquery"
 	"context"
-	"github.com/cottand/selfhosted/dev-go/lib/bedrock"
 	"github.com/cottand/selfhosted/dev-go/lib/secretstore"
 	"golang.org/x/oauth2"
 	"google.golang.org/api/option"
@@ -24,7 +23,6 @@ func (s *vaultOAuthTokenSource) Token() (*oauth2.Token, error) {
 		s.latest = renewed
 	}
 
-	a := bedrock.Result[string, string]{}
 	return &oauth2.Token{
 		AccessToken: s.latest.Token,
 		Expiry:      s.latest.ExpiresAt,
