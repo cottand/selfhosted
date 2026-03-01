4e107c62d56abc74b7d345639a2b32e7e11a0242
Cottand <nico.dc@outlook.com>
2026-03-01 18:15:31 +0000

    set suggested mqtt opts

--- dev-go/services/s-rpc-mqtt/module.go
+++ dev-go/services/s-rpc-mqtt/module.go
@@ -3,6 +3,7 @@ package module
 import (
 	"context"
 	"log/slog"
+	"time"
 
 	"github.com/cottand/selfhosted/dev-go/lib/bedrock"
 	"github.com/cottand/selfhosted/dev-go/lib/config"
@@ -29,18 +30,15 @@ func InitService() (*bedrock.Service, string, error) {
 	opts := mqtt.NewClientOptions()
 	opts.SetClientID(Name + "_" + baseConfig.AllocID)
 	opts.AddBroker(brokerAddr)
+	// see https://github.com/eclipse-paho/paho.mqtt.golang?tab=readme-ov-file#common-problems
+	opts.SetOrderMatters(false)
+	opts.SetKeepAlive(5 * time.Second)
+	opts.SetAutoReconnect(true)
 
 	ctx, cancel := context.WithCancel(ctx)
 	opts.SetConnectionLostHandler(func(client mqtt.Client, err error) {
 		slog.WarnContext(ctx, "mqtt connection lost", "brokerAddr", brokerAddr, "err", err)
 
-		go func() {
-			router := &mqttRouter{c: client, clientId: opts.ClientID}
-			err := router.start(ctx)
-			if err != nil {
-				slog.ErrorContext(ctx, "failed to start mqtt router", "brokerAddr", brokerAddr, "err", err)
-			}
-		}()
 	})
 
 	client := mqtt.NewClient(opts)
