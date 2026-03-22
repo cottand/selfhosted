f5a4729d71bbef343bbbcd301dd6b985da677b21
Cottand <nico.dc@outlook.com>
2026-03-22 19:35:09 +0000

    autoscaling metrics

--- dev-go/services/s-rpc-autoscaler/process_job.go
+++ dev-go/services/s-rpc-autoscaler/process_job.go
@@ -3,13 +3,33 @@ package module
 import (
 	"context"
 
+	"github.com/cottand/selfhosted/dev-go/lib/util"
 	nomad "github.com/hashicorp/nomad/api"
 	"github.com/monzo/terrors"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
 const (
 	autoscalingValueDisabled = "disabled"
 	autoscalingValueEnabled  = "enabled"
+
+	targetCPUPercentage    = 0.6
+	targetMemoryPercentage = 0.8
+)
+
+var (
+	targetCPUMetric = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: util.KebabToSnakeCase(name),
+		Name:      "task_target_cpu",
+		Help:      "Target CPU for task",
+	}, []string{"nomad_job", "task_name", "task_group"})
+
+	targetMemoryMetric = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: util.KebabToSnakeCase(name),
+		Name:      "task_target_memorymb",
+		Help:      "Target memory for task",
+	}, []string{"nomad_job", "task_name", "task_group"})
 )
 
 func isEligibleForAutoscaling(meta map[string]string) (bool, error) {
@@ -26,14 +46,6 @@ func isEligibleForAutoscaling(meta map[string]string) (bool, error) {
 
 func processJob(ctx context.Context, def *nomad.Job) error {
 	errParams := map[string]string{"jobId": *def.ID}
-	ok, err := isEligibleForAutoscaling(def.Meta)
-	if err != nil {
-		return terrors.Augment(err, "failed to check autoscaling status", errParams)
-	}
-	if !ok {
-		return nil
-	}
-
 	for _, group := range def.TaskGroups {
 		for _, task := range group.Tasks {
 			ok, err := isEligibleForAutoscaling(task.Meta)
@@ -43,6 +55,21 @@ func processJob(ctx context.Context, def *nomad.Job) error {
 			if !ok {
 				continue
 			}
+
+			targetCPU := targetCPUPercentage * float64(*task.Resources.CPU)
+			targetMemory := targetMemoryPercentage * float64(*task.Resources.MemoryMB)
+
+			targetCPUMetric.With(prometheus.Labels{
+				"nomad_job":  *def.ID,
+				"task_name":  task.Name,
+				"task_group": *group.Name,
+			}).Set(targetCPU)
+
+			targetMemoryMetric.With(prometheus.Labels{
+				"nomad_job":  *def.ID,
+				"task_name":  task.Name,
+				"task_group": *group.Name,
+			}).Set(targetMemory)
 
 		}
 	}
